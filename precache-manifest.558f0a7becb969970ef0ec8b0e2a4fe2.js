464bb56d86b517acffcaa682cf189f14cddba214
Gustavo Pospi <gutxander@gmail.com>
2022-11-04 14:18:21 -0300

    Updates

---
+++ precache-manifest.558f0a7becb969970ef0ec8b0e2a4fe2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a1c29ce790a5437e7e8edfbb6966849",
+    "url": "/app-toot-name/index.html"
+  },
+  {
+    "revision": "7c60b35bf12190cc8f45",
+    "url": "/app-toot-name/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6f5862da6c37d3cf79d2",
+    "url": "/app-toot-name/static/js/2.54495ddb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/app-toot-name/static/js/2.54495ddb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c60b35bf12190cc8f45",
+    "url": "/app-toot-name/static/js/main.64ba1def.chunk.js"
+  },
+  {
+    "revision": "66ac581fbc8990623b1d",
+    "url": "/app-toot-name/static/js/runtime-main.cfc30c8d.js"
+  }
+]);
